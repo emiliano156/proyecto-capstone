09e2d4942b65c866da11c50f50466f15404627c4
emiliano156 <149023552+emiliano156@users.noreply.github.com>
2024-07-12 12:51:52 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+
+
+//inicializa tu base de datos
+const firebaseConfig = {
+  apiKey: "AIzaSyCHvtK_87omCaYl6zDFuhdWEI3_Sn5y2C4",
+  authDomain: "proyecto-b72fe.firebaseapp.com",
+  databaseURL: "https://proyecto-b72fe-default-rtdb.firebaseio.com",
+  projectId: "proyecto-b72fe",
+  storageBucket: "proyecto-b72fe.appspot.com",
+  messagingSenderId: "414275402557",
+  appId: "1:414275402557:web:06814bbdfb941ab9e2a354",
+  measurementId: "G-GSXKSPGL1W"
+};
+
+  
+
+  
